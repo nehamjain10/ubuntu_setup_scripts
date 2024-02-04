00f1f154e60d15bd4b6ff3e8a95628033897c1d3
Neham Jain <63699813+nehamjain10@users.noreply.github.com>
2024-02-03 19:20:14 -0500

    setup

--- conda_setup.sh
+++ conda_setup.sh
@@ -3,10 +3,10 @@
 # Install Anaconda
 echo "Installing Anaconda..."
 wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
-bash Anaconda3-2023.09-0-Linux-x86_64 -b -p $HOME/anaconda3
+bash Anaconda3-2023.09-0-Linux-x86_64.sh -b -p $HOME/anaconda3
 export PATH="$HOME/anaconda3/bin:$PATH"
 rm Anaconda3-2023.09-0-Linux-x86_64.sh
 
 # Initialize conda
 conda init zsh
-source ~/.zshhrc
+source ~/.zshrc
